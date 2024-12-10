0844818d0e8cb14560147088e01fa6a4cb81871a
Stephany <stephanypalacio5487@gmail.com>
2024-12-10 13:45:40 -0500

    backend3

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
-    console.log('Conectado a MongoDB');
-  } catch (err) {
-    console.error('Error en la conexión:', err.message);
-    process.exit(1); // Salir con error
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Conectado a MongoDB');
+    } catch (err) {
+        console.error('Error en la conexión:', err.message);
+        process.exit(1); // Salir con error
+    }
 };
 
 module.exports = connectDB;
