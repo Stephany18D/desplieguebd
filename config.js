d78b7cef9f02dddccb9a38a9617b278f2e71792f
Stephany <stephanypalacio5487@gmail.com>
2024-12-10 15:17:39 -0500

    backend4

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    const uri = process.env.MONGODB_URL;
+    console.log("MONGODB_URL:", uri); // Verifica que la variable esté definida
+    
+    if (!uri) {
+        console.error("MONGODB_URL no está definida.");
+        process.exit(1); // Salir con error
+    }
+    
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(uri);
         console.log('Conectado a MongoDB');
     } catch (err) {
         console.error('Error en la conexión:', err.message);
